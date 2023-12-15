f9598c49bff3ffe44ae9dfdb1011b092ac307052
francisco <franciscosimoes2002@hotmail.com>
2023-12-15 11:44:22 +0000

    Modificado e ainda não está totamente o css do site

--- includes/header.php
+++ includes/header.php
@@ -3,8 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="view/styles.css">
-    <link href="https://fonts.googleapis.com/css2?family=Orbitron&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="view/css/style.css">
     <title>Rebel Retro Refuge</title>
 </head>
 <body>
@@ -12,28 +11,23 @@
         <h1>Rebel Retro Refuge</h1>
     </header>
 
-    <nav>
-        <?php
-            $scriptName = basename($_SERVER['PHP_SELF']);
-        ?>
-        <ul>
-            <li><a href="index.php"
-                <?php echo $scriptName == 'index.php' ? "class='corrente'" : '' ?>
-            >Inicio</a></li>
-            <li><a href="arcades.php"
-                <?php echo $scriptName == 'index.php' ? "class='corrente'" : '' ?>
-            >Arcades</a></li>
-            <li><a href="contactos.php"
-                <?php echo $scriptName == 'index.php' ? "class='corrente'" : '' ?>
-            >Contactos</a></li>
-        </ul>
+    <div class="sidebar">
+        <nav>
+            <?php 
+                if (isset($_SESSION['username'])) {
+                    echo '<div class="user-info">Bem-vindo, ' . $_SESSION['username'] . '!<br><a href="logout.php">Logout</a></div>';
+                } else {
+                    echo '<div class="user-info"><a href="login.php">Login</a> <a href="register.php">Register</a></div>';
+                }
 
-        <?php 
-            if (isset($_SESSION['username'])) {
-                echo '<div class="user-info">Bem-vindo, ' . $_SESSION['username'] . '!<br><a href="logout.php">Logout</a></div>';
-            } else {
-                echo '<div class="user-info"><a href="login.php">Login</a> | <a href="register.php">Register</a></div>';
-            }
+                $scriptName = basename($_SERVER['PHP_SELF']);
+            ?>
 
-        ?>
-    </nav>
+            <ul>
+                <li><a href="index.php" <?php echo $scriptName == 'index.php' ? "class='corrente'" : '' ?>>Inicio</a></li>
+                <li><a href="arcades.php" <?php echo $scriptName == 'arcades.php' ? "class='corrente'" : '' ?>>Arcades</a></li>
+                <li><a href="contactos.php" <?php echo $scriptName == 'contactos.php' ? "class='corrente'" : '' ?>>Contactos</a></li>
+            </ul>
+
+        </nav>
+    </div>
--- view/indexView.php
+++ view/indexView.php
@@ -1,6 +1,6 @@
-
-<section id="content">
-        Content will be dynamically loaded here based on user actions
-        <h2>Bem vindo ao Rebel Retro Refuge</h2>
-        <p>Descobre uma escapatório rebeliosa para o mundo dos jogos retro!</p>
-</section>
+<div class="content">
+        <section>
+                <h2>Bem vindo ao Rebel Retro Refuge</h2>
+                <p>Descobre uma escapatório rebeliosa para o mundo dos jogos retro!</p>
+        </section>
+</div>
