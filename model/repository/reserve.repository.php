2aacd4fbd31519a0137d7821acfcfa513d03a46a
Tiago <>
2023-12-15 10:01:15 +0000

    Completar a classe e a lógica das funções createReservation e cancelReservation, para comunicar com o ArcadeReservation.repository

--- model/repository/reserve.repository.php
+++ model/repository/reserve.repository.php
@@ -1,11 +1,33 @@
 <?php
 require ("acessoDB.php");
 
-function reserveArcade($arcadeModel, $userId){
+function createReservation($userId, $arcade_id){
+    $conn = connectDB();
+    $stmt = $conn->prepare("INSERT INTO reserva ('utilizador_id') VALUES (:utilizador_id)");
+    $stmt->execute( [ 'utilizador_id' => $userId] );
 
+    $reservationId = end(getReservations());
+
+    addArcadeToReservation($reservationId, $arcade_id);
 }
 
 function cancelReservation($reservationId){
+    $conn = connectDB();
+    $stmt = $conn->prepare("DELETE FROM reserva WHERE (:id_reserva)");
+    $stmt->execute( [ 'id_reserva' => $reservationId ] );
+}
 
+function getReservations(){
+    $conn = connectDB();
+    $stmt = $conn->query("SELECT * FROM reserva");
+    $reservations = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+	return $reservations;
+}
+
+function getReservation($reservationId){
+    $conn = connectDB();
+    $stmt = $conn->prepare("SELECT FROM reserva WHERE (:id_reserva)");
+    $stmt->execute( [ 'id_reserva' => $reservationId ] );
 }
 ?>
