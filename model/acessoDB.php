c7a77d36729fbb7d8ef360769147e69ef21e5d67
Tiago <>
2023-12-15 09:28:21 +0000

    Add funções no user.repository e tirar essa logica do acessoDB

--- model/acessoDB.php
+++ model/acessoDB.php
@@ -17,20 +17,4 @@ function connectDB() {
 
     return $conn;
 }
-
-function addUser( $username, $pass ) {
-    $conn = connectDB();
-    $stmt = $conn->prepare("INSERT INTO utilizador ('username', 'pass') VALUES (:username, :pass)");
-    $stmt->execute( [ 'username' => $username, 'pass' => $pass ] );
-}
-
-function userExistes( $username, $pass ) {
-    $conn = connectDB();
-    $stmt = $conn->prepare("SELECT username FROM utilizador WHERE username=:username and pass=:pass");
-    $stmt->execute( [ 'username' => $username, 'pass' => $pass ] );
-
-
-}
-
-
 ?>
---
+++ model/repository/user.repository.php
@@ -0,0 +1,22 @@
+<?php
+require ("acessoDB.php");
+
+function addUser( $username, $pass ) {
+    $conn = connectDB();
+    $stmt = $conn->prepare("INSERT INTO utilizador ('username', 'pass') VALUES (:username, :pass)");
+    $stmt->execute( [ 'username' => $username, 'pass' => $pass ] );
+}
+
+
+function deleteUser($userId){
+    
+}
+
+function userExistes( $username, $pass ) {
+    $conn = connectDB();
+    $stmt = $conn->prepare("SELECT username FROM utilizador WHERE username=:username and pass=:pass");
+    $stmt->execute( [ 'username' => $username, 'pass' => $pass ] );
+
+
+}
+?>
