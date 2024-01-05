b76ab0488602403648fa2b80f91c7fe61af50cc3
Tiago <>
2024-01-05 09:25:34 +0000

    Adicionar profile view

--- view/profileView.php
+++ view/profileView.php
@@ -1,3 +1,28 @@
 <div class="profile"> 
+    <?php   
+    $idUtilizador = null;
+    $reservas = getReservations($idUtilizador);
+    foreach ($reservas as $reserva) {
+        $id_reserva = $reserva['id_reserva'];
+        $username = $reserva['username'];
+        $id_arcade = $reserva['id_reserva'];
+        $hora_reserva = $reserva['hora_reserva'];
 
+        $arcade = getArcade($id_arcade);
+        echo <<<LISTA
+        form
+        <ul>
+            <li>"Reserva: " $id_reserva</li>
+            <li>"Hora: " $hora_reserva</li>
+            <li>"Arcade: " $arcade</li>
+            <button onclick=cancelarReserva($idUtilizador, $id_reserva)>Cancelar</button> 
+        </ul>
+        LISTA;
+    }
+
+    function cancelReserva($idUtilizador, $id_reserva){
+        cancelReservation($idUtilizador, $id_reserva);
+    }
+
+    ?>
 </div>
