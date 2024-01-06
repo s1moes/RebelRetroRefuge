d8992a9b5e3a12ee7ad733f134d30d74feb0ff5d
Tiago <>
2024-01-06 20:58:23 +0000

    Apagar css antigo

---
+++ arcade.php
@@ -0,0 +1,10 @@
+<?php
+
+    session_start();
+
+    include('model/acessoDB.php');
+    include('includes/header.php');
+    include('view/arcadeView.php');
+    include('includes/footer.php');
+?>
+
