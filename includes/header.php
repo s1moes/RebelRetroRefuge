1c7a20f3c9a8d8fda67a6ea3a6aa6a0caa101e2b
Tiago <>
2024-01-06 18:32:34 +0000

    Adicionar header com bootstrap

--- includes/header.php
+++ includes/header.php
@@ -3,26 +3,31 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="view/css/style.css">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <title>Rebel Retro Refuge</title>
 </head>
 <body>
-    <div class="main">
+    <nav class="navbar navbar-expand-lg bg-body-tertiary" data-bs-theme="dark">
+        <div class="container-fluid">
             <?php $scriptName = basename($_SERVER['PHP_SELF']); ?>
-            <a href="/RebelRetroRefuge/index.php"><img src="icon.png" alt="Logotipo" class="logotipo"></a>
-            <ul class="primary-nav">
-                <li class="nav-tab"><a href="index.php" <?php echo $scriptName == 'index.php' ? "class='corrente'" : '' ?>>Inicio</a></li>
-                <li class="nav-tab"><a href="arcades.php" <?php echo $scriptName == 'arcades.php' ? "class='corrente'" : '' ?>>Arcades</a></li>
-                <li class="nav-tab"><a href="sobre.php" <?php echo $scriptName == 'sobre.php' ? "class='corrente'" : '' ?>>Sobre</a></li>
-            </ul>
-        <div class="second-nav">
+            <a class="navbar-brand" href="index.php">RebelRetroRefuge</a>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav">
+                    <a class="nav-link" href="index.php" <?php echo $scriptName == 'index.php' ? 'class="nav-link active" aria-current="page"' : '' ?>>Inicio</a>
+                    <a class="nav-link" href="arcades.php" <?php echo $scriptName == 'arcades.php' ? 'class="nav-link active" aria-current="page"' : '' ?>>Arcades</a>
+                    <a class="nav-link" href="sobre.php" <?php echo $scriptName == 'sobre.php' ? 'class="nav-link active" aria-current="page"' : '' ?>>Sobre</a>
+                </div>  
+            </div>
+            <div class="navbar-nav">
                 <?php 
                     if (isset($_SESSION['username'])) {
-                        echo '<div class="user-info">Bem-vindo, <a href="profile.php">' . $_SESSION['username'] .'</a>!<br><br><a href="logout.php">Logout</a></div>';
+                        echo '<a class="nav-link" href="profile.php.php">Bem-vindo, ' . $_SESSION['username'] .'</a>';
+                        echo '<a class="nav-link" href="logout.php">Logout</a>';
                     } else {
-                        echo '<div class="user-info"><a href="login.php">Login</a> <a href="register.php">Register</a></div>';
+                        echo '<a class="nav-link" href="login.php">Login</a>'; 
+                        echo '<a class="nav-link" href="register.php">Register</a>';
                     }
                 ?>
+            </div>  
         </div>
-    </div>
+    </nav>
