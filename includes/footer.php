05cb50a68f568e2444d8c0b04c4eafebda80e0c9
Tiago <>
2024-01-06 18:32:52 +0000

    Adicionar footer com bootstrap

--- includes/footer.php
+++ includes/footer.php
@@ -1,5 +1,16 @@
-    <footer>
-        <p>&copy; 2023 Rebel Retro Refuge</p>
+    <footer class="p-3 mb-2 bg-dark text-white">
+        <div class="container text-center ">
+                <div class="row align-items-center">
+                        <div class="col">
+                        </div>
+                        <div class="col-6">
+                            <p>&copy; 2023 Rebel Retro Refuge</p>
+                        </div>
+                        <div class="col">
+                        </div>
+                </div>
+        </div>
     </footer>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
 </html>
