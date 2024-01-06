37c528cc2eb9cb3fa80ca4fdd8327511ccec4c12
Tiago <>
2024-01-06 18:35:18 +0000

    Adicionar indexView com bootstrap

--- view/indexView.php
+++ view/indexView.php
@@ -1,7 +1,15 @@
-        
-        <div class="content">
-            <section>
-                    <h2>Bem vindo ao Rebel Retro Refuge</h2>
-                    <p>Diverte-te ao máximo com as nossas máquinas arcade!</p>
-            </section>
+<div class="bg-image" style="background-image: url('https://i.pinimg.com/564x/15/b8/25/15b825780d23bcc5db366c78afd239bb.jpg');">
+        <div class="container text-center">
+                <div class="row align-items-center d-flex align-items-center">
+                        <div class="col">
+                        </div>
+                        <div class="col-6 p-3 m-3 bg-dark text-white" >
+                                <h2 class="text-white">Bem vindo ao Rebel Retro Refuge</h2>
+                                <p class="text-white">Diverte-te ao máximo com as nossas máquinas arcade!</p>
+                                <img src="https://i.pinimg.com/564x/e3/28/6f/e3286f7ab68e867893a87e1780022dd3.jpg" alt="">
+                        </div>
+                        <div class="col">
+                        </div>
+                </div>
         </div>
+</div>
