ae4b00de8eb4d906cf93010c5d8e83c557b99cd6
TMTomas <126092390+TMTomas@users.noreply.github.com>
2024-01-07 15:16:16 +0000

    Adicionar bootstrap

--- view/sobreView.php
+++ view/sobreView.php
@@ -1,3 +1,18 @@
-    <div class="sobre">
-        <h3>Queremos boa experiência</h3>
-    </div>
+<div class="bg-image min-vh-100">
+        <div class="container text-center">
+                <div class="row align-items-center d-flex align-items-center">
+                        <div class="col">
+                        </div>
+                        
+                        <div class="col-6 p-3 m-3 bg-dark text-white rounded" >
+                                <h3 class="text-white">Trabalho realizado no âmbito da disciplina de Programação Web por:</h3>
+                                <h5 class="text-white">Francisco Simões e Tiago Tomás</h5>
+                                <img src="https://i.pinimg.com/564x/1e/bb/97/1ebb97adcb4516c000520876e1197c87.jpg" alt="">
+                                <h3 class="text-white">O intuito do site é construir uma sistema de reserva de máquinas arcades de um estabelecimento</h3>
+                                
+                        </div>
+                        <div class="col">
+                        </div>
+                </div>
+        </div>
+</div>
