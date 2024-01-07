f7c72d184a50e78e9e067fcbbf79c8db497241ae
TMTomas <126092390+TMTomas@users.noreply.github.com>
2024-01-07 15:13:00 +0000

    Alteração do esquema

--- view/indexView.php
+++ view/indexView.php
@@ -5,9 +5,9 @@
                         </div>
                         
                         <div class="col-6 p-3 m-3 bg-dark text-white rounded" >
-                                <h1 class="text-white">Bem vindo ao Rebel Retro Refuge</h1>
-                                <p class="text-white">Diverte-te ao máximo com as nossas máquinas arcade!</p>
+                                <h1 class="text-white m-3">Bem vindo ao Rebel Retro Refuge</h1>
                                 <img src="https://i.pinimg.com/564x/e3/28/6f/e3286f7ab68e867893a87e1780022dd3.jpg" alt="">
+                                <p class="text-white m-3">Diverte-te ao máximo com as nossas máquinas arcade!</p>
                         </div>
                         <div class="col">
                         </div>
