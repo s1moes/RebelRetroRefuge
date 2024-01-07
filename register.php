9e58292142b86f41d6bbb98b8f5fa3f353e924b7
TMTomas <126092390+TMTomas@users.noreply.github.com>
2024-01-07 16:14:47 +0000

    Separar login do loginView

--- register.php
+++ register.php
@@ -4,34 +4,8 @@
         header('Location: index.php');
         exit;
     }
+    include('includes/header.php');
+    include('view/registerView.php');
+    include('includes/footer.php');
 ?>
-<!DOCTYPE html>
-<html lang="pt">
-<head>
-    <meta charset="UTF-8">
-    <title>Registro - Rebel Retro Refuge</title>
-    <link rel="stylesheet" href="view/register.css">
-</head>
-<body>
-    <form action="register_process.php" method="post">
-        <h2>Registro</h2>
-        <?php
-            if (isset($_GET['error']) && $_GET['error'] == 'true') {
-                echo '<p style="color: red;">Erro ao registrar. Tente novamente.</p>';
-            }
-        ?>
-        <label for="username">Username</label>
-        <input type="text" id="username" name="username" required>
 
-        <label for="password">Password</label>
-        <input type="password" id="password" name="password" required>
-
-        <label for="confirm_password">Confirme a password</label>
-        <input type="password" id="confirm_password" name="confirm_password" required>
-
-        <button type="submit">Registrar</button>
-
-        <p>Já tem conta? <a href="login.php">Clique aqui!</a></p>
-    </form>
-</body>
-</html>
