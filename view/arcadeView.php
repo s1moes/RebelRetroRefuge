eea6971a2d25ac14b34f5dd49b0acd987fcdfcaf
TMTomas <126092390+TMTomas@users.noreply.github.com>
2024-01-07 15:06:40 +0000

    Finalizar a classe

--- view/arcadeView.php
+++ view/arcadeView.php
@@ -1,7 +1,5 @@
 <?php
-if (isset($_POST["id_arcade"])) {
-  $idArcade = $_POST["id_arcade"];
-}
+$idArcade = isset($_POST["id_arcade"]) ? $_POST["id_arcade"] : '';
 
 $arcade = getArcade($idArcade);
 $arcadeNome = $arcade[0]["nome"];
@@ -17,14 +15,21 @@ $arcadeDescricao = $arcade[0]["descricao"];
                         <div class="col-6 p-3 m-3 bg-dark text-white rounded" >
                                 <h1 class="text-white"><?php echo $arcadeNome ?></h1>
                                 <img src="<?php echo $arcadeImagem ?>" width="512px" alt="">
-                                
                         </div>
                         <div class="col p-3 m-3 bg-dark text-white">
                                 <p class="text-white"><?php echo $arcadeDescricao ?></p>  
                                 <form action="reserva.php" method="post">
-                                <input type="datetime-local" name="datetime">
+                                <label for="date">Seleccione a data e hora da reserva:</label>
+                                <input type="date" name="date" min="<?php echo date("Y-m-d"); ?>" required>
+                                <select name="time" required>
+                                        <?php
+                                        for ($hora = 9; $hora <= 17; $hora++) {
+                                                echo "<option value='$hora'>$hora:00 - " . ($hora + 1) . ":00</option>";
+                                        }
+                                        ?>
+                                </select>
                                 <input type="hidden" name="idArcade" value="<?php echo $idArcade ?>">
-                                <button type="submit"> Reservar</button>
+                                <button type="submit" class="btn btn-outline-light m-3">Reservar</button>
                                 </form>      
                         </div>
                 </div>
