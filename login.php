266dc58d48d7ccc276dd80529238cc8b68958c26
TMTomas <126092390+TMTomas@users.noreply.github.com>
2024-01-07 16:14:34 +0000

    Separar login do loginView

--- login.php
+++ login.php
@@ -4,31 +4,7 @@
         header('Location: index.php');
         exit;
     }
+    include('includes/header.php');
+    include('view/loginView.php');
+    include('includes/footer.php');
 ?>
-<!DOCTYPE html>
-<html lang="pt">
-<head>
-    <meta charset="UTF-8">
-    <title>Login - Rebel Retro Refuge</title>
-    <link rel="stylesheet" href="view/login.css">
-</head>
-<body>
-    <form action="login_process.php" method="post">
-        <h2>Login</h2>
-        <?php
-            if (isset($_GET['error']) && $_GET['error'] == 'true') {
-                echo '<p style="color: red;">Credenciais inválidas. Tente novamente.</p>';
-            }
-        ?>
-        <label for="username">Username</label>
-        <input type="text" id="username" name="username" required>
-
-        <label for="password">Password</label>
-        <input type="password" id="password" name="password" required>
-
-        <button type="submit">Login</button>
-
-        <p>Ainda não tem conta? <a href="register.php">Clique aqui!</a></p>
-    </form>
-</body>
-</html>
